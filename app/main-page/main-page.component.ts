983681aab94a4d3f237ad5108f93f2be18f7749c
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2020-05-07 14:06:31 -0400

    Bold keywords for experiences

--- app/main-page/main-page.component.ts
+++ app/main-page/main-page.component.ts
@@ -39,22 +39,28 @@ export class MainPageComponent {
 
   experienceList = [
     {
-      jobTitle: 'Software Developer',
-      date: 'Jun. 2020 - Sept. 2021',
-      company: 'XYZ Corp',
-      description: 'Built a bunch of shit',
+      jobTitle: 'Web Interface Engineer (Co-op)',
+      date: 'Sep 2019 - Apr 2020',
+      company: 'Intersect',
+      list: [
+              `Feature development working with a <b>large agile software development team</b> building an <b>enterprise application with Angular</b> (started with Angular 8, stayed the migration path to the latest version)`,
+              `Involvement on team included using <b>Typescript</b>, <b>HTML</b>, <b>SCSS</b> and <b>Angular</b> in order to build an average of 2 major features per sprint`,
+              `Reduced redundancies in the code base and maintained project consistency by creating <b>reusable Angular components</b> for UIs such as navigation bars and card widgets`,
+              `Leveraged <b>Apollo GraphQL</b> in order to mock data which allowed for the rapid creation of functional UIs without waiting for the back-end to be developed`,
+              `Supported the development of a process for building and validating features for <b>accessibility compliance</b> to <b>WCAG level 2.1</b> for the project`,
+              `Tested for <b>accessibility</b> and <b>cross-browser compatibility</b> during development and code reviews by using a <b>screen reader</b>, <b>aXe scans</b> and <b>BrowserStack</b>`,
+            ],
     },
     {
-      jobTitle: 'Software Engineer',
+      jobTitle: 'Web Applications Developer (Co-op)',
       date: 'Jun. 2020 - Sept. 2021',
-      company: 'XYZ Corp',
-      description: 'Built a bunch of shit',
-    },
-    {
-      jobTitle: 'Web Developer',
-      date: 'Jun. 2020 - Sept. 2021',
-      company: 'XYZ Corp',
-      description: 'Built a bunch of shit',
+      company: 'Environment canada',
+      list: [
+            `Used <b>Typescript</b>, <b>HTML</b>, <b>SCSS</b>, <b>AngularJS</b> and the <b>ArcGIS API</b> to build a map integration that allows users to bookmark geographic areas as well as view relevant data about their bookmarked areas`,
+            `Supported the development of a responsive datatable that supports user file uploads and URLs in a variety of formats (<b>CSV</b>, <b>JSON</b> and <b>Shapefile</b>) and presents the data in an interactive and easy-to-digest manner`,
+            `Took initiative to document accessibility guidelines in order to ensure compliance with  <b>WCAG level 2.0</b>, which cut down QA reports of accessibility-related bugs in half`,
+            `Wrote <b>unit tests using Jasmine</b> as well as <b>UI tests using WebdriverIO</b> to detect flaws in logic`,
+      ],
     },
   ];
 
