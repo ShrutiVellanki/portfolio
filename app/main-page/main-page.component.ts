823f6f32ce6b061132c5a87e2b11acd875a2dd8a
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2020-05-07 16:22:52 -0400

    Clean up color scheme

--- app/main-page/main-page.component.ts
+++ app/main-page/main-page.component.ts
@@ -39,9 +39,20 @@ export class MainPageComponent {
 
   experienceList = [
     {
-      jobTitle: 'Web Interface Engineer (Co-op)',
+      jobTitle: 'Front-End Developer',
+      date: 'Jun 2020 - Present',
+      company: 'Environment Canada',
+      employmentType: 'Full Time',
+      list: [
+              `I just started this job! Check out other sections for more detailed descriptions.`,
+      ],
+      skills: [],
+    },
+    {
+      jobTitle: 'Web Interface Engineer',
       date: 'Sep 2019 - Apr 2020',
       company: 'Intersect',
+      employmentType: 'Internship',
       list: [
               `Feature development working with a <b>large agile software development team</b> building an <b>enterprise application with Angular</b> (started with Angular 8, stayed the migration path to the latest version)`,
               `Involvement on team included using <b>Typescript</b>, <b>HTML</b>, <b>SCSS</b> and <b>Angular</b> in order to build an average of 2 major features per sprint`,
@@ -49,18 +60,21 @@ export class MainPageComponent {
               `Leveraged <b>Apollo GraphQL</b> in order to mock data which allowed for the rapid creation of functional UIs without waiting for the back-end to be developed`,
               `Supported the development of a process for building and validating features for <b>accessibility compliance</b> to <b>WCAG level 2.1</b> for the project`,
               `Tested for <b>accessibility</b> and <b>cross-browser compatibility</b> during development and code reviews by using a <b>screen reader</b>, <b>aXe scans</b> and <b>BrowserStack</b>`,
-            ],
+      ],
+      skills: ['HTML', 'SCSS', 'Typescript', 'Angular', 'Apollo GraphQL', 'Karma', 'BrowserStack'],
     },
     {
-      jobTitle: 'Web Applications Developer (Co-op)',
-      date: 'Jun. 2020 - Sept. 2021',
-      company: 'Environment canada',
+      jobTitle: 'Front-End Developer',
+      date: 'May 2018 - Aug 2019',
+      company: 'Environment Canada',
+      employmentType: 'Internship',
       list: [
             `Used <b>Typescript</b>, <b>HTML</b>, <b>SCSS</b>, <b>AngularJS</b> and the <b>ArcGIS API</b> to build a map integration that allows users to bookmark geographic areas as well as view relevant data about their bookmarked areas`,
             `Supported the development of a responsive datatable that supports user file uploads and URLs in a variety of formats (<b>CSV</b>, <b>JSON</b> and <b>Shapefile</b>) and presents the data in an interactive and easy-to-digest manner`,
             `Took initiative to document accessibility guidelines in order to ensure compliance with  <b>WCAG level 2.0</b>, which cut down QA reports of accessibility-related bugs in half`,
             `Wrote <b>unit tests using Jasmine</b> as well as <b>UI tests using WebdriverIO</b> to detect flaws in logic`,
       ],
+      skills: ['HTML', 'SCSS', 'Typescript', 'AngularJS', 'WebdriverIO', 'Jasmine', 'ArcGIS API'],
     },
   ];
 
