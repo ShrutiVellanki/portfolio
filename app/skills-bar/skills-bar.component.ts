e3e7f4fcdad0b08d7220be94afef54c7e851d911
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2020-06-21 11:20:12 -0400

    Animate skill bar

--- app/skills-bar/skills-bar.component.ts
+++ app/skills-bar/skills-bar.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit, Input } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-skills-bar',
   templateUrl: './skills-bar.component.html',
   styleUrls: ['./skills-bar.component.scss']
 })
-export class SkillsBarComponent {
+export class SkillsBarComponent implements OnInit {
 
   @Input() skills;
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+        const element = document.getElementsByClassName('skills-bar-wrapper')[0];  
+        if (element && this.isInViewport(element)) {
+          $(".mat-progress-bar").each(function(){
+            $(this).find(".bar-inner").animate({
+              width: $(this).attr("data-width")
+            },2000)
+          });
+        }
+    });
+  }
+
+  isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+
+    return (
+      rect.top >= -300 &&
+      rect.top <= 1000 &&
+      rect.left >= 0 &&
+      rect.right <= (window.innerWidth || html.clientWidth)
+    );
+  }
 
 }
