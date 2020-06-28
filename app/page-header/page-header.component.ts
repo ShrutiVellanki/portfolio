92fd11b0c7a6519a44e3918277513aad034ee0eb
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2020-06-28 18:50:22 -0400

    Underlines for nav bar

--- app/page-header/page-header.component.ts
+++ app/page-header/page-header.component.ts
@@ -8,12 +8,66 @@ import { MediaMatcher } from '@angular/cdk/layout';
 })
 export class PageHeaderComponent implements AfterViewInit {
   @ViewChild('snav') sidenav: any;
-
   ngAfterViewInit() {
-    console.log(this.sidenav);
+    document.getElementById
   }
 
   get opened() {
     return this.sidenav && this.sidenav.opened;
+  }
+
+  scrollToView(element, closeSideNav = false) {
+    const scrollToEl = document.querySelector(element);
+    const headerHeight = 50;
+    const buffer = 25;
+  
+    const topOfElement = window.pageYOffset + scrollToEl.getBoundingClientRect().top - headerHeight - buffer;
+
+    window.scroll({
+      top: topOfElement,
+      behavior: 'smooth' 
+    });
+
+    if (closeSideNav) {
+      this.sidenav.toggle();
+    }
+  }
+
+  get introInView() {
+    const intro = document.getElementById('intro').children[0];
+    const skills = document.getElementById('skills').children[0];
+    return this.inViewPort(intro) && !this.inViewPort(skills);
+  }
+
+  get skillsInView() {
+    const skills = document.getElementById('skills').children[0];
+    const experience = document.getElementById('experience').children[0];
+
+    return this.inViewPort(skills) && !this.inViewPort(experience);
+  }
+
+  get experienceInView() {
+    const projects = document.getElementById('projects').children[0];
+    const experience = document.getElementById('experience').children[0];
+
+    return this.inViewPort(experience) && !this.inViewPort(projects);
+  }
+
+  get projectsInView() {
+    const projects = document.getElementById('projects').children[0];
+    const contact = document.getElementById('contact').children[0];
+
+    return this.inViewPort(projects) && !this.inViewPort(contact);
+  }
+
+  get contactInView() {
+    const contact = document.getElementById('contact').children[0];
+
+    return this.inViewPort(contact);
+  }
+
+  inViewPort(element) {
+    const bounding = element.getBoundingClientRect();
+    return bounding.top <= 100;
   }
 }
