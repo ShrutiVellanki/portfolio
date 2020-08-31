06cd729af8055b447a3c70625e0583396f7a1cc8
ShrutiVellanki <shruti.vellanki@mail.utoronto.ca>
2020-08-30 21:46:31 -0400

    Get form submission reset working

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, FormGroupDirective } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -10,17 +9,36 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-  message = new FormControl('', [Validators.required]);
-  phone = new FormControl('', [Validators.required]);
-  subject = new FormControl('', [Validators.required]);
-  firstName = new FormControl('', [Validators.required]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  contactForm;
   api = 'https://mailthis.to/shruti.vellanki.96@gmail.com';
   validate = false;
 
-  constructor(private http: HttpClient, private builder: FormBuilder, private _snackBar: MatSnackBar) { }
+  constructor(private http: HttpClient, private builder: FormBuilder, private _snackBar: MatSnackBar) {
+    this.contactForm = this.builder.group({
+      message: new FormControl('', [Validators.required]),
+      subject: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  get subject() {
+    return this.contactForm.controls.subject;
+  }
+
+  get email() {
+    return this.contactForm.controls.email;
+  }
+
+  get firstName() {
+    return this.contactForm.controls.firstName;
+  }
+
+  get message() {
+    return this.contactForm.controls.message;
   }
 
   errorMsg() {
@@ -31,48 +49,14 @@ export class ContactComponent implements OnInit {
     return this.email.hasError('email') ? `That's not an email...` : '';
   }
 
-  processForm() {    
-    this.openToastResetForm()
-
-  }
-
-
-  openToastResetForm() {
-    this._snackBar.open('Email Sent!', 'OK', {
-      duration: 2000,
-    });
-
-    this.firstName.reset();
-    this.email.reset()
-    this.subject.reset()
-    this.message.reset()
-    Array.from([this.firstName, this.email, this.subject, this.message]).forEach(field => { 
-       field.markAsUntouched({ onlySelf: true });
-    });
-  }
-
-  FormData() {
-    this.builder.group({
-      FullName: this.firstName,
-      Email: this.email,
-      Subjec: this.subject,
-      Message: this.message
-    })
-  }
-
-  PostMessage(input: any) {
-    return this.http.post(this.api, input, { responseType: 'text'}).pipe(
-      map(
-        (response) => {
-          console.log(response)
-          if (response) {
-            return response
-          }
-        },
-        (error: any) => {
-          return error;
-        }
-      )
-    )
+  processForm(event) {    
+    console.log(event)
+    if(this.contactForm.valid) {
+      this._snackBar.open('Email Sent!', 'OK', {
+        duration: 2000,
+      });
+      event.target.reset()
+      this.contactForm.reset()
+    }
   }
 }
